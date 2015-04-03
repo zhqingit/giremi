2aff013d6dcdd2351738587008f00bc4aeea8fbe
zhqingit <zhqingfu@gmail.com>
2015-04-03 08:46:25 -0700

    Fix the confusing ifRNAE mark

--- giremi.pl
+++ giremi.pl
@@ -21,4 +21,7 @@ if ($#ARGV>0){
 	my $cmd = "R CMD BATCH --no-save --no-restore '--args finput=\"$output\" fout=\"$output.res\"' giremi.r $output.Rout";
 	print $cmd,"\n";
 	system($cmd);
+
+	print "Analysis DONE!";
 }
+
